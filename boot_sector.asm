1a6e2bec49e3422b4288e133eb3b36f49cbf4cfa
geekofia <chandu.chandaniit@gmail.com>
2018-10-09 13:32:38 +0530

    Simple Bootloader

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,4 +1,4 @@
 jmp $ ;creates loop - $
 
-times 510 -($-$$) db 0
-dw 0xaa55 ;Not a checksum, but more of a signature. It does provide some simple evidence that some MBR is present.
+times 510 -($-$$) db 0 ;Zerofill up to 510 bytes
+dw 0xaa55 ;Boot Sector signature
